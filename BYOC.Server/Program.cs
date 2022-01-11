d5e68adbd99aa048a701597c505bf8408b315fbc
Stick <stick.mick@gmail.com>
2022-01-11 22:32:28 +1000

    Scaffold identity Remove DB from repo Basic server setup

--- BYOC.Server/Program.cs
+++ BYOC.Server/Program.cs
@@ -28,7 +28,6 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services
     .AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 // Placeholder for adding game stuff
 
